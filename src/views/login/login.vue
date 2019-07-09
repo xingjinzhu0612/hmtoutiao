9227ce5db65e04d21f34c1f16de7dfa826fd6c07
xingjinzhu0612 <1018715459@qq.com>
2019-07-09 17:27:52 +0800

    绘制表单

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,37 +1,62 @@
 <template>
-    <div class="login-contsiner">
-        <el-card class="login_box">
-            <img src="../../assets/images/logo_index.png" alt="">
-        </el-card>
-    </div>
+  <div class="login-contsiner">
+    <!-- 卡片 -->
+    <el-card class="login_box">
+      <img src="../../assets/images/logo_index.png" alt />
+      <!-- 表单 -->
+      <el-form>
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入验证码" style="width:250px"></el-input>
+          <el-button style='float:right'>发送验证码</el-button>
+        </el-form-item>
+         <el-form-item>
+          <el-checkbox v-model="checked"></el-checkbox> 我已阅读并同意 <el-link :underline="false" type="primary">用户协议</el-link>和 <el-link :underline="false" type="primary">隐私条款</el-link>
+        </el-form-item>
+         <el-form-item>
+             <el-button style='width:100% ' type='primary'>登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+    data(){
+        return{
+            checked:true
+        }
+    }
+};
 </script>
 
 <style scoped lang='less' >
 .login-contsiner {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .login_box {
+    width: 420px;
+    height: 350px;
     position: absolute;
-    left:0;
-    top:0;
-    background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
-        .login_box{
-            width: 420px;
-            height: 320px;
-            position: absolute;
-            left: 50%;
-            top:50%;
-            transform: translate(-50%,-50%);
-            img {
-                display: block;
-                width: 200px;
-                margin: 0 auto;
-            }
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 10px auto 30px;
     }
+  }
+}
+
+.el-checkbox {
+  margin-right: 10px;  
 }
 </style>
