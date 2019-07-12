54470acebfe8aadf74eb0394b6bc559d70d947b5
xingjinzhu0612 <1018715459@qq.com>
2019-07-12 16:06:31 +0800

    导航跳转与404

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import Login from '../views/login/login.vue'
 import Home from '../views/home/home.vue'
 import Welcome from '../views/welcome/welcome.vue'
+import NotFound from '../views/404/NotFound.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -13,7 +14,8 @@ const router = new VueRouter({
       children: [
         { name: 'welcome', path: '/', component: Welcome }
       ]
-    }
+    },
+    { name: '404', path: '*', component: NotFound }
 
   ]
 })
--- src/views/welcome/welcome.vue
+++ src/views/welcome/welcome.vue
@@ -10,16 +10,20 @@ export default {}
 
 <style scoped lang='less'>
 .welcome-container {
+  // display: table-cell;
+  // text-align: center;
+  // vertical-align: middle;
     position: relative;
     // width: 100%;
     // height:600px;
     // background: url(../../assets/images/welcome.jpg) no-repeat center
     img {
-        // display: block;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%,50%);
-    }
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,50%);
+    // display: inline-block;
+  }
 }
 </style>
